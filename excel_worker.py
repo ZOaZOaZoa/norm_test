5705c8623c4b85c592c020cd66ac72298e33033d
ZOaZOaZoa <vbifbk555@gmail.com>
2024-06-01 14:57:29 +0300

    excel work done

---
+++ excel_worker.py
@@ -0,0 +1,66 @@
+import norm_test
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from math import ceil
+
+def get_col_no_blank(df, col_name):
+    k = pd.Series(df[df[col_name].apply(lambda x: type(x) in [int, np.int64, float, np.float64])][col_name]).values.astype(np.float64)
+    k = k[~np.isnan(k)]
+    k = k[k!=0]
+
+    return k
+
+def main():
+    while True:
+        print('Введите название (или путь) файла Excel с данными: ', end='')
+        file = input()
+        if os.path.isfile(file):
+            print(f'\nОткрываем файл {file}')
+            break
+
+        print(f'Файла {file} не существует')
+
+
+    xl = pd.ExcelFile(file)
+    sheets = xl.sheet_names
+
+    if len(sheets) == 0:
+        print('Не найдено листов в файле')
+        exit()
+
+    while True:
+        print('Найдены следующие листы: ', end='')
+        print(*sheets, sep='; ')
+        if len(sheets) == 1:
+            print(f'Выбран лист {sheets[0]}\n')
+            sheet = sheets[0]
+            break
+
+        print('Введите название листа, откуда брать данные: ', end='')
+        sheet = input()
+        if sheet in sheets:
+            print(f'Выбран лист {sheets[0]}\n')
+            break
+
+        print(f'Листа {sheet} среди найденных листов нет')
+
+    print('Парсим данные')
+    df = xl.parse(sheet)
+    
+    print('Найдены следующие столбцы:', *df.columns)
+    print('Строим графики. Если pvalue > 0.05, значит критерий на нормальность выполняется')
+    col_chunks = np.array_split(df.columns, ceil(len(df.columns) / 4))
+    for k, chunk in enumerate(col_chunks):
+        print(f'{k+1} график из {len(col_chunks)}')
+        for i, col in enumerate(chunk):
+            k = get_col_no_blank(df, col)
+            plt.subplot(2,2, i+1)
+            chisquare_pvalue, ks_pvalue = norm_test.plot_fit(k, col, isSubplot=True)
+            print(f'{col:<30} Критерий пирсона: {chisquare_pvalue:<8.3f} Критерий Колмогорова-Смирнова: {ks_pvalue:<8.3f}')
+        plt.show()
+        print()
+
+if __name__ == '__main__':
+    main()
--- norm_test.py
+++ norm_test.py
@@ -2,39 +2,36 @@ import scipy.stats as sps
 import numpy as np
 import matplotlib.pyplot as plt
 
-def chisquare_test(data, plot=True):
+def plot_fit(data, title="", isSubplot=False):
     data_mean = np.mean(data)
     data_std = np.std(data)
 
     #Теоретическое распределение
-    if plot:
-        x = np.linspace(min(data), max(data), 100)
-        plt.plot(x, sps.norm.pdf(x, loc=data_mean, scale=data_std), color='k', label='theoretical')
+    x = np.linspace(min(data), max(data), 100)
+    plt.plot(x, sps.norm.pdf(x, loc=data_mean, scale=data_std), 'k--', label='Теоретическая\nподгонка')
     
     #Диаграмма накопленных частот
-    (observed, bins, _) = plt.hist(data, bins = round(1 + 3.2*np.log10(len(data))), density=True, color='g', label='observed')
-
-    if not plot:
-        plt.close()
+    (observed, bins, _) = plt.hist(data, bins = round(1 + 3.2*np.log10(len(data))), density=True, label='Наблюдения')
 
     bin_centers = [ (bins[i] + bins[i+1])/2 for i in range(len(bins) - 1) ]
     bin_width = bins[1] - bins[0]
 
     #Ожидаемая диаграмма накопленных частот
     expected = np.array([ sps.norm.pdf(bin_c, loc=data_mean, scale=data_std) for bin_c in bin_centers ])
-    #Корректировка, чтобы суммы накопленных частот были равны (требуется для теста)
-    expected *= np.sum(observed)/np.sum(expected)
-    
-    #Критерий согласия Пирсона (хи-квадрат)
-    statistic, pvalue = sps.chisquare(observed, expected, ddof=2)
 
-    if plot:
-        plt.bar(bin_centers, height=expected, width=0.4*bin_width, color='orange', label='expected')
-        plt.legend()
-        plt.title(f'Statistic: {statistic:.2f}, pvalue: {pvalue:.2f}')
+    _, chisquare_pvalue = chisquare_test(data)
+    _, ks_pvalue = ks_test(data)
+
+    plt.bar(bin_centers, height=expected, width=0.4*bin_width, color='orange', label='Ожидаемое')
+    plt.legend()
+    plt.title(f'{title}\nКритерий Пирсона pvalue - {chisquare_pvalue:.3f}\nКритерий Колмогорова-Смирнова pvalue - {ks_pvalue:.3f}')
+    if not isSubplot:
         plt.show()
     
-    return statistic, pvalue
+    return (chisquare_pvalue, ks_pvalue)
+
+def chisquare_test(data):
+    return sps.normaltest(data)
 
 def ks_test(data):
     mu = np.mean(data)
@@ -46,12 +43,7 @@ def main():
     #data = sps.norm.rvs(size=100, loc=12, scale=13)   
     #data = sps.norm.rvs(size=10000)
     data = sps.norm.rvs(size=100, loc=14, scale=10)
-    statistic, pvalue = chisquare_test(data, plot=False)
-    print(f'Statistic: {statistic:.2f}, pvalue: {pvalue:.2f}')
-
-    print('\nKstest')
-    statistic, pvalue = ks_test(data)
-    print(f'Statistic: {statistic:.2f}, pvalue: {pvalue:.2f}')
+    plot_fit(data)
 
 if __name__ == '__main__':
     main()
